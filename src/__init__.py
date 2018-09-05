9edfc1da082d818900a04037181a6f806880fcf8
xquercus <jeff@mossycup.com>
2018-09-05 16:52:26 -0400

    Add support for Anki 2.1 standard scheduler

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from . import load_balanced_scheduler 
---
+++ src/load_balanced_scheduler.py
@@ -0,0 +1,34 @@
+import sys
+import anki
+from anki.sched import Scheduler
+
+_debug_lvl = 2
+
+
+def dbg(message, level):
+    if _debug_lvl >= level:
+        sys.stdout.write(message)
+
+
+def load_balanced_ivl(self, ivl):
+    """Return the (largest) interval that has the least number of cards and falls within the 'fuzz'"""
+    orig_ivl = int(ivl)
+    min_ivl, max_ivl = self._fuzzIvlRange(orig_ivl)
+    min_num_cards = 18446744073709551616        # Maximum number of rows in an sqlite table?
+    best_ivl = 1
+    for check_ivl in range(min_ivl, max_ivl + 1):
+        num_cards = self.col.db.scalar("""select count() from cards where due = ? and queue = 2""",
+                                       self.today + check_ivl)
+        if num_cards <= min_num_cards:
+            best_ivl = check_ivl
+            dbg("> ", 2)
+            min_num_cards = num_cards
+        else:
+            dbg("  ", 2)
+        dbg("check_ivl {0:<4} num_cards {1:<4} best_ivl {2:<4}\n".format(check_ivl, num_cards, best_ivl), 2)
+    dbg("* orig_ivl {0:<4} min_ivl {1:<4} max_ivl {2:<4} best_ivl {3:<4}\n".format(orig_ivl, min_ivl, max_ivl, best_ivl), 1)
+    return best_ivl
+
+
+old_fuzzedIvl = anki.sched.Scheduler._fuzzedIvl
+anki.sched.Scheduler._fuzzedIvl = load_balanced_ivl
