e41d5064fef3ab37bf63a80c9b04137926762da0
xquercus <jeff@mossycup.com>
2018-09-10 11:23:31 -0400

    Add support for Anki 2.1 v2 scheduler

--- src/load_balanced_scheduler.py
+++ src/load_balanced_scheduler.py
@@ -1,6 +1,8 @@
 import sys
 import anki
 from anki.sched import Scheduler
+from anki.schedv2 import Scheduler
+
 
 _debug_lvl = 2
 
@@ -26,9 +28,11 @@ def load_balanced_ivl(self, ivl):
         else:
             dbg("  ", 2)
         dbg("check_ivl {0:<4} num_cards {1:<4} best_ivl {2:<4}\n".format(check_ivl, num_cards, best_ivl), 2)
-    dbg("* orig_ivl {0:<4} min_ivl {1:<4} max_ivl {2:<4} best_ivl {3:<4}\n".format(orig_ivl, min_ivl, max_ivl, best_ivl), 1)
+    dbg("* orig_ivl {0:<4} min_ivl {1:<4} max_ivl {2:<4} best_ivl {3:<4} ****\n".format(orig_ivl, min_ivl, max_ivl, best_ivl), 1)
     return best_ivl
 
 
-old_fuzzedIvl = anki.sched.Scheduler._fuzzedIvl
 anki.sched.Scheduler._fuzzedIvl = load_balanced_ivl
+
+
+anki.schedv2.Scheduler._fuzzedIvl = load_balanced_ivl
